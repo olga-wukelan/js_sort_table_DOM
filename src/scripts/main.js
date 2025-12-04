d66b0e52919baa1ed00787af83281dc0e7a694cd
Olga Wukelan <olga.wukelan@gmail.com>
2025-12-04 11:20:08 +0400

    create a file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,57 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+const tbody = table.querySelector('tbody');
+const allRows = document.querySelectorAll('tbody tr');
+
+headers.forEach((th, i) => {
+  th.setAttribute('data-order', 'asc');
+
+  th.addEventListener('click', () => {
+    sortByColumn(i, th);
+  });
+});
+
+function sortByColumn(columnIndex, headersElem) {
+  const currentOrder = headersElem.getAttribute('data-order');
+  const newOrder = currentOrder === 'asc' ? 'desc' : 'asc';
+
+  const sortedRows = Array.from(allRows).sort((rowA, rowB) => {
+    const cellA = rowA.children[columnIndex];
+    const cellB = rowB.children[columnIndex];
+
+    const textA = cellA.textContent.trim();
+    const textB = cellB.textContent.trim();
+
+    const valA =
+      columnIndex === 2 || columnIndex === 3
+        ? parseFloat(textA.replace('$', '').replace(',', ''))
+        : textA;
+
+    const valB =
+      columnIndex === 2 || columnIndex === 3
+        ? parseFloat(textB.replace('$', '').replace(',', ''))
+        : textB;
+
+    let comparison = 0;
+
+    if (valA < valB) {
+      comparison = -1;
+    } else if (valA > valB) {
+      comparison = 1;
+    }
+
+    if (newOrder === 'desc') {
+     return comparison * -1;
+  }});
+
+
+  headers.forEach((h) => h.removeAttribute('data-order'));
+
+  headersElem.setAttribute('data-order', newOrder);
+
+  sortedRows.forEach((row) => {
+    tbody.appendChild(row);
+  });
+}
